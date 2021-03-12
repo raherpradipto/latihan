20060b273b6cc1774e6f0a7a06699f8710133b28
raherpradipto <80144773+raherpradipto@users.noreply.github.com>
2021-03-12 14:01:42 +0700

    Add files via upload

---
+++ I0320083_Exercise2.2.py
@@ -0,0 +1,30 @@
+#proses memasukan data ke dalam variabel
+nama = "Rahmat Herpradipto"
+#proses mencetak variabel
+print(nama)
+
+#nilai dan tipe data dalam variabel dapat diubah
+umur = 19               #nilai awal
+print(umur)             #mencetak nilai umur
+type(umur)              #mengecek tipe data umur
+umur = "Sembilan Belas" #nilai setelah diubah
+print(umur)             #mencetak nilai umur
+type(umur)              #mengecek tipe data umur
+
+namaDepan = "Rahmat"
+namaBelakang = "Herpradipto"
+nama = namaDepan + " " + namaBelakang
+umur = 19
+hobi = "Olahraga"
+print("Biodata\n", nama, "\n", umur, "\n", hobi)
+
+#contoh variabel lainya
+inivariabel = "Halo"
+ini_juga_variabel = "Hai"
+_inivariabeljuga = "Hi"
+inivariabel222 = "Bye"
+
+panjang = 10
+lebar = 5
+luas = panjang * lebar
+print(luas)
